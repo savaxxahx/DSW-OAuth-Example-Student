4917c0658eef3714ec4088ee2bb02d4ada5b91e9
savaxxahx <savannah@katherynsavannah.com>
2018-02-12 09:59:42 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -80,7 +80,7 @@ def renderPage1():
 @app.route('/page2')
 def renderPage2():
     if 'user_data' in session:
-        user_data_pprint = pprint.pformat(session['user_data']['public_repos'])#format the user data nicely
+        user_data_pprint = " 'public_repos'" + pprint.pformat(session['user_data']['public_repos'])#format the user data nicely
     else:
         user_data_pprint = '';
     return render_template('page1.html',dump_user_data=user_data_pprint)
