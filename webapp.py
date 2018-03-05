070e90170bba5be7373de3596b2ebfbd8a6f8940
savaxxahx <savannah@katherynsavannah.com>
2018-03-05 09:50:01 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -56,7 +56,7 @@ def authorized():
     resp = github.authorized_response()
     if resp is None:
         session.clear()
-        flash('Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args))      
+        flash("'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)")      
     else:
         try:
             #save user data and set log in message
