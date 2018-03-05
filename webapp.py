765d8c4838e2fbf82480e37d5359830b05285d6b
savaxxahx <savannah@katherynsavannah.com>
2018-03-05 09:33:38 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -61,11 +61,11 @@ def authorized():
             #save user data and set log in message
             session['github_token']=(resp['access_token'],'')
             session['user_data']=github.get('user').data
-            flash("You have successfully logged in as")
+            flash("You have successfully logged in as ")
         except:
             #clear the session and give error message
             session.clear()
-            flash(“You were not logged in try again”)
+            flash("You were not logged in try again")
     return render_template('message.html', message=message)
 
 
